c1bec7d060f73a62c0747f0cf9d8844bfb47adb5
gabri3lly <gabriellyaparecida.2000gmail.com>
2024-05-23 08:34:05 -0300

    editando tela (css)

--- src/screens/Task_List.js
+++ src/screens/Task_List.js
@@ -1,11 +1,17 @@
 import React, {Component} from "react"
-import { StyleSheet, View, Text } from "react-native"
+import { StyleSheet, View, Text, ImageBackground } from "react-native"
+import today_Image from '../../assets/imgs/today.jpg'
 
 export default class Task_List extends Component {
     render(){
         return(
             <View styele ={styles.container}>
-                <Text>Lista de Tarefas</Text>
+                <ImageBackground source={today_Image} style={styles.background}>
+
+                </ImageBackground>
+                <View style={styles.tasksList}>
+                    <Text>Lista de Tarefas</Text>
+                </View>
             </View>
         )
     }
@@ -14,5 +20,11 @@ export default class Task_List extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1
+    },
+    background:{
+        flex: 3
+    },
+    tasksList:{
+        flex: 7
     }
 })
